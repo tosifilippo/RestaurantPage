d0a57a9f1c86eafb31c613cedeb746ad21cb4337
Filippo <tosifilippo@outlook.it>
2021-02-23 04:21:47 +0100

    placed menu img in a grid

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import loadHomePage from './modules/home-page.js';
+import loadButtons from './modules/buttons.js';
 import loadContactPage from './modules/contact.js';
+import loadMenuPage from './modules/menu.js';
+
+function handleEvents() {
+    const homeButton = document.getElementById("homeButton");
+    const contactButton = document.getElementById("contactButton");
+    const menuButton = document.getElementById("menuButton");
+
+    homeButton.addEventListener("click", loadHomePage);
+    contactButton.addEventListener("click", loadContactPage);
+    menuButton.addEventListener("click", loadMenuPage);
+}
 
 loadHomePage();
-loadContactPage();
+loadButtons();
+handleEvents();
